caa4df1dd119639c7d789fe23e2a4dc25702c043
Estevao <estevaobutilheiroo@gmail.com>
2020-12-15 15:52:50 +0000

    commit on 'React-Course'

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,44 +1,76 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import './App.css';
-import Person from './Person/Person.js';
+import Person from './Person/Person';
 
-const app = props => {
+class App extends Component {
+  state = {
+    persons: [
+      { name: 'Max', age: 28 },
+      { name: 'Manu', age: 29 },
+      { name: 'Stephanie', age: 26 }
+    ],
+    otherState: 'some other value'
+  };
 
-  const [ personsState, setPersonsState ] = useState({
-      persons : [
-        { name: 'Max', age: 28 },
-        { name: 'Manu', age: 29},
-        { name: 'Stephanie', age: 28}
-      ]
-  });
-
-  const [otherState, setOtherstate] = useState('some other value');
-
-  console.log(personsState, otherState);
-
-  const switchNameHandler = () =>{
-    //DONT DO THIS this.state.persons[0].name = 'Estevao';
-    setPersonsState( {
-      persons : [
-        { name: 'Maxine', age: 28 },
-        { name: 'Manu', age: 29},
-        { name: 'Stephanie', age: 21}
+  switchNameHandler = (newName) => {
+    // console.log('Was clicked!');
+    // DON'T DO THIS: this.state.persons[0].name = 'Maximilian';
+    this.setState({
+      persons: [
+        { name: newName, age: 28 },
+        { name: 'Manu', age: 29 },
+        { name: 'Stephanie', age: 27 }
       ]
     });
   };
 
-  return (
-    //Em React (JSX), elementos precisam ter pelo menos um "pai". Por exemplo, não posso criar outro <h1> fora da div App.
-    <div className="App">
-      <h1>Hi, I'm a React app</h1>
-      <p>This is really Working!</p>
-      <button onClick={switchNameHandler}>Switch Name</button>
-      <Person name={personsState.persons[0].name} age={personsState.persons[0].age}>My Hobbies are RACING!</Person>
-      <Person name={personsState.persons[1].name} age={personsState.persons[1].age}/>
-      <Person name={personsState.persons[2].name} age={personsState.persons[2].age}/>
-    </div>
-  );
-    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi! I\'m a React App!!!'));
+  nameChangedHandler = (event) =>{
+    this.setState({
+      persons: [
+        { name: 'Max', age: 28 },
+        { name: event.target.value, age: 29 },
+        { name: 'Stephanie', age: 26 }
+      ]
+    });
+  }
 
-  };
-  export default app;
+  render() {
+    const style ={
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    };
+
+
+    return (
+      <div className="App">
+        <h1>Hi, I'm a React App</h1>
+        <p>This is really working!</p>
+        <button 
+          style={style}
+          onClick={() => this.switchNameHandler('Maximilian!!')}>Switch Name</button>
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age}
+        />
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+          click ={this.switchNameHandler.bind(this, 'Max!')}
+          changed = {this.nameChangedHandler}
+        >
+          My Hobbies: Racing
+        </Person>
+        <Person
+          name={this.state.persons[2].name}
+          age={this.state.persons[2].age}
+        />
+      </div>
+    );
+    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
+  }
+}
+
+export default App;
--- react-complete-guide/src/Person/Person.js
+++ react-complete-guide/src/Person/Person.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import './Person.css';
 
 const person = (props) => {
     //Essa const é a forma de criar funções no ES6, que é o que eu deveria usar sempre.
     return (
-        <div>
-            <p>I'm {props.name} and I am {props.age} years old!</p>
+        <div className="Person">
+            <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
             <p>{props.children}</p>
+            <input typer="text" onChange={props.changed} value={props.name}/>
         </div>
     )
 }
